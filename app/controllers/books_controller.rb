1d10af8cfa43e1ac47fa4b89b862205db30f3a97
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-09-29 10:36:06 +0000

    [finish]いいね順

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
     @book = Book.new
-    @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
   end
 
   def create
@@ -44,11 +49,11 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-end
 
   def ensure_correct_user
-      @book = Book.find(params[:id])
-      unless @book.user == current_user
-        redirect_to books_path
-      end
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
+end 
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,8 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
+  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
+  
   
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
